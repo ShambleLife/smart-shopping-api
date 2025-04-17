6262612e8c1c0ad35c8fae9eb50b584fda3784ac
ShambleLife <92964026+ShambleLife@users.noreply.github.com>
2025-04-17 13:23:10 -0400

    Testing Price Scraping

--- docs/shop-results.js
+++ docs/shop-results.js
@@ -13,7 +13,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   );
 
   const [krogerData, publixData, wholefoodsData] = await Promise.all([
-    fetch("https://simple-grocery-store-api.glitch.me/products").then(res => res.json()),
+    fetch("https://corsproxy.io/?https://simple-grocery-store-api.glitch.me/products")
+.then(res => res.json()),
     fetch("publix.json").then(res => res.json()),
     fetch("wholefoods.json").then(res => res.json())
   ]);
